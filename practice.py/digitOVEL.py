c54c6b80459f8a958c6d83d4ae19ac5d250161dc
divyani <divyanikumbar@gmail.com>
2023-08-19 23:22:23 +0530

    added

---
+++ practice.py/digitOVEL.py
@@ -0,0 +1,95 @@
+#const last vowels 1st
+# l=['a','t','i','k','c','v','o','n','d','p']
+# l2=[]
+# l3=[]
+# for i in l:
+#     if(i=='a' or i=='e' or i=='i' or i=='o' or i=='u'  ):
+#         l2.append(i)
+#     else:
+#         l3.append(i)
+# l4=l2+l3
+# for i in l4:
+#     print(i,end=' ')
+
+# separte const vowels and number
+# s="2gi20cs040divyani"
+# l=list(s)
+# l1=[]
+# l2=[]
+# l3=[]
+# c1,c2,c3=0,0,0
+# for i in l:
+#     if i.isdigit():
+#         c1+=1
+#         l1.append(i)
+#     elif i.isalpha():
+#         if(i=='a' or i=='e' or i=='i' or i=='o' or i=='u'  ):
+#             c2+=1
+#             l2.append(i)
+#         else:
+#             c3+=1
+#             l3.append(i)
+# print("numbers.......>>>>>",end=" ")
+# for i in l1:
+#     print(i,end=" ")
+# print("\n total numbers",c1)
+# print()
+# print("vowels.......>>>>>",end=" ")
+# for i in l2:
+#     print(i,end=" ")
+# print("\n total numbers",c2)
+# print()
+# print("consonants.......>>>>>",end=" ")
+# for i in l3:
+#     print(i,end=" ")
+# print("\n total numbers",c3)
+   
+   
+#remove number from string
+# s="2gi20cs040divyani2002gh7ujkk"
+# l=list(s)
+# l2=[]
+# for i in l:
+#     if i.isalpha():
+#         l2.append(i)
+# for i in l2:
+#     print(i,end="")   #we cant use remove function as it removes 1st occurrence 
+
+    
+#using join and split
+# l=['divya','kumbar','preeth','giryalkar']
+# s='-'.join(l)
+# print(s)
+
+
+# s="divyakumbar prithamgityalkar"
+# l=s.split(" ")
+# print(l)
+
+
+
+        
+        
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
